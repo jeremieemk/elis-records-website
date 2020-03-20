dab9a1e96c9c15c46df94c0d84b20b6be0b23497
jeremiemk <jeremieemk@gmail.com>
2020-03-20 09:24:45 +0000

    release-page ok

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "521171f1e34039dbb245";
+/******/ 	var hotCurrentHash = "b10b2fe5a4d331ef41ea";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
