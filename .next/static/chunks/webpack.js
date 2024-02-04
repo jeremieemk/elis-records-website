88fc9d2c0428f417a3fa23b93e1212626c45807a
Jérémie <jeremieemk@gmail.com>
2024-02-04 17:49:47 +0100

    deploy bug fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f971aae79bc40462"; }
+/******/ 		__webpack_require__.h = function() { return "bf108b924fe7bb08"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
